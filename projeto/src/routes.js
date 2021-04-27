2b66db7212e2fce8cb98b5a8cf10bd2d7f2f6d51
Ezequiel Moreira <ezequielmoreira@cpejr.com>
2021-04-27 20:56:58 -0300

    Routes

---
+++ projeto/src/routes.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Cadastro from "./paginas/Cadastro";
+import Empreeendimentos from "./paginas/Empreendimentos";
+import Empresas from "./paginas/Empresas";
+import Home from "./paginas/Home";
+import Login from "./paginas/Login";
+import Perfil from "./paginas/Perfil";
+import QuemSomos from "./paginas/QuemSomos";
+
+
+function Routes(){
+    return(
+<BrowserRouter>
+<Switch>
+    
+<Route/>
+<Route/>
+<Route/>
+
+</Switch>
+
+
+
+</BrowserRouter>
+
+
+
+    );
+}
