587f075c8b32f0ad07330830f395c09d0d36be87
Ezequiel Moreira <ezequielmoreira@cpejr.com>
2021-04-29 15:13:57 -0300

    Criar CSS Global

--- projeto/src/paginas/QuemSomos/QuemSomos.js
+++ projeto/src/paginas/QuemSomos/QuemSomos.js
@@ -4,28 +4,38 @@ import "./QuemSomos.css"
 function QuemSomos(){
   return (
 
-  <div className="Pagina">  
-  <img className="quemSomos" src="/imagem/QuemSomos.jpg" alt=""/>
-  <h2 className = "Subtitulo"> Empresa: </h2>
-  <img src="/imagem/foto.jpg" alt=""/>
+<div className="PaginaInteira">
+
+
+
+
+<div className = "Cabeçalho">
+
+<img src="/imagem/QuemSomos.jpg" alt=""/>
+<p> A Hora da Fruta fornece frutas e snacks saudáveis para ambientes corporativos e, para explicar como funciona seu negócio, colocou um vídeo na página de about.
+A animação explica como fazer pedidos, como são selecionados os produtos e como a empresa que contratou o serviço os recebe, além de dar uma ideia sobre a missão e os valores da Hora da Fruta.
+Esse recurso é interessante para negócios complexos ou novos, que ainda não são conhecidos do grande público. </p>
+</div>
+
+
 
 <div className = "Meio"> 
 <img src="/imagem/foto.jpg" alt=""/>
 <h1> Pessoas são assim </h1>
 </div>
 
-<div> 
+<div className = "ParteFinal"> 
 <img src="/imagem/duvidas.png" alt=""/>
+
       <h1> Dúvidas Frequentes</h1>
+
+      <h2> O que você fazer quando você gosta do nosso site? </h2>
+      <p> A gente só pode agradecer. </p>
       <h2> O que você fazer quando você gosta do nosso site? </h2>
       <p> A gente só pode agradecer. </p>
 
+
 </div>
-
-
-
-
-
 
     </div>
   );
